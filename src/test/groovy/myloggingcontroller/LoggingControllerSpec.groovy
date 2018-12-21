cb657cb9a4bc0da96b515627922b531c65a48e48
Jean Tessier <jean@jeantessier.com>
2018-12-21 11:14:05 -0800

    Simple controller that writes to logs, with test

---
+++ src/test/groovy/myloggingcontroller/LoggingControllerSpec.groovy
@@ -0,0 +1,25 @@
+package myloggingcontroller
+
+import grails.test.mixin.TestFor
+import org.slf4j.Logger
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(LoggingController)
+class LoggingControllerSpec extends Specification {
+
+    def setup() {
+        controller.log = Mock(Logger)
+    }
+
+    void "test something"() {
+        when:
+        controller.index()
+
+        then:
+        1 * controller.log.error(_)
+    }
+
+}
